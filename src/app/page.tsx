17a7d3d7463b3f219be8b499ebed0751dbee388a
Abdullah Ahmed <joed72427@gmail.com>
2025-08-04 06:40:56 +0500

    add tawk to

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+// page.tsx
+"use client";
+import React from 'react';
+import Script from 'next/script';
+
 import Navbar from './components/navbar/Navbar';
 import HeroContent from './components/landingPage/HeroContent';
 import CardCarousel from './components/HomeCard/CardCarouselParent';
@@ -8,20 +12,42 @@ import AIInputCard from './components/HomeCard/AIInputCard';
 import AnimationCard from './components/HomeCard/AnimationCard';
 import Paragraph from './components/HomeCard/Paragraph';
 
-const page = () => {
+const Page = () => {
   return (
-    <div>
-      <Navbar />
-      <HeroContent />
-      <CardCarousel />
-      <MinimalCardCarousel />
-      <Paragraph />
-      <SkiperCardPage />
-      <AIInputCard />
-      <AnimationCard />
-      <Paragraph />
-    </div>
-  )
-}
+    <>
+      {/* Tawk.to Script */}
+      <Script
+        id="tawk-to"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/68900c09d4de331929811800/1j1pau1tp';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+        }}
+      />
 
-export default page
+      {/* Page Content */}
+      <div>
+        <Navbar />
+        <HeroContent />
+        <CardCarousel />
+        <MinimalCardCarousel />
+        <Paragraph />
+        <SkiperCardPage />
+        <AIInputCard />
+        <AnimationCard />
+        <Paragraph />
+      </div>
+    </>
+  );
+};
+
+export default Page;
